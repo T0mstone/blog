2898fac640910e5325e6897a13c373804adbb288
T0mstone <realt0mstone@gmail.com>
2020-04-22 19:59:42 +0200

    changed the post system in two ways: 	1. posts now operate using metadata commands rather than the limited split-title approach used before 	2. posts are now displayed latest-first and with their post number and language on the index page

--- compiler.py
+++ compiler.py
@@ -1,20 +1,35 @@
 import random
 from pathlib import Path
 
+def maybe_get(d, k):
+	return d[k] if k in d else None
+
+def extract_post_metatdata(s):
+	def inner(line):
+		spl = tuple(s.strip() for s in line.split(' ', 1))
+		return spl if len(spl) == 2 else (spl[0], None)
+
+	return dict(inner(line[1:]) for line in s.split('\n') if line.startswith('!'))
+
 def process(s):
+	s = '\n'.join(line for line in s.split('\n') if not line.startswith('!'))
 	s = s.replace('\\\n', '').replace('\n', '<br />')
 	return s
 
-def add_boilerplate(cont, title, subtitle=None):
+def add_boilerplate(cont, meta):
 	indent1 = ' '*8
 	indent2 = ' '*12
+
+	title = meta['title']
+	subtitle = maybe_get(meta, 'subtitle')
+	tabtitle = maybe_get(meta, 'tabtitle')
 
 	subtitle = "" if subtitle is None else f"\n{indent1}<center class=\"faint\">\n{indent2}{subtitle}\n{indent1}</center>"
 	return f"""<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8" />
-	<title> {title} - Tom's Blog</title>
+	<title> {title if tabtitle is None else tabtitle} - Tom's Blog</title>
 	<link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 <body>
@@ -27,37 +42,51 @@ def add_boilerplate(cont, title, subtitle=None):
 </body>
 </html>"""
 
-def split_post_src(s):
-	spl = tuple(s.split('\n', 1))
-	head, text = spl if len(spl) == 2 else (spl[0], '')
-	spl = tuple(s.strip() for s in head.split('--', 1))
-	title, subtitle = spl if len(spl) == 2 else (spl[0], None)
-	return title, subtitle, text
-
 def list_post_srcs():
-	return [path for path in Path('src').iterdir() if path.name not in ['.DS_Store', 'index_src.html']]
+	return [path for path in Path('src').iterdir() if path.name != '.DS_Store']
 
 def make_hub():
-	def inner(template, quote, posts):
-		post_str = ('\n' + ' '*8).join(f'<a href="post/{filename}">{title}</a>' for filename, title in posts)
-		return template.replace('%quote', quote).replace('%posts', post_str)
+	def make_entry(filename, meta):
+		title = meta['title']
+		flag = get_flag(meta['lang'])
+		n = meta['n']
 
-	posts = [(path.stem + '.html', split_post_src(path.read_text())[0]) for path in list_post_srcs()]
+		return f'<p class="post-link">[{flag}] ({n}) <a href="post/{filename}">{title}</a></p>'
+
 	quotes = Path('quotes.txt').read_text().split('\n')
 	selected_quotes = [line[2:] for line in quotes if line.startswith('> ')]
 	if len(selected_quotes) == 0:
 		quote = quotes[random.randrange(len(quotes))]
 	else:
 		quote = selected_quotes[0]
-	with open('src/index_src.html', 'r') as f:
+	
+	with open('index_src.html', 'r') as f:
 		template = f.read()
-	return inner(template, quote, posts)
+
+	posts = [(path.stem + '.html', extract_post_metatdata(path.read_text())) for path in list_post_srcs()]
+	posts = sorted(posts, key=lambda t: int(t[1]['n']), reverse=True)
+	post_str = ('\n' + ' '*8).join(make_entry(filename, meta) for filename, meta in posts)
+
+	return template.replace('%quote', quote).replace('%posts', post_str)
+
+def get_flag(code):
+	code = code.lower()
+	if code == 'en':
+		code = 'gb'
+	# this makes use of unicode's flag handling to translate literally any country code into a unicode flag emoji
+	code_a = 127462
+	return ''.join([chr(code_a + ord(c) - ord('a')) for c in code])
 
 def main():
 	for path in list_post_srcs():
 		print(f'compiling {path.name}...')
-		title, subtitle, text = split_post_src(path.read_text())
-		res = add_boilerplate(process(text), title, subtitle)
+		text = path.read_text()
+		meta = extract_post_metatdata(text)
+		for (k, e) in {'title': 'title', 'lang': 'language annotation', 'n': 'post number'}.items():
+			if k not in meta:
+				print(f'"{path.stem}" is missing a {e}')
+				continue
+		res = add_boilerplate(process(text), meta)
 		filename = path.stem + '.html'
 		path2 = Path('post') / filename
 		path2.write_text(res)
